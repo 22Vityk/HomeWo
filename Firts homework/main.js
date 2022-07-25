4bab8886c8a70978bee532dc9d6e2a03e888a1ea
22Vityk <Vityk2904@gmail.com>
2022-07-25 14:32:56 +0300

    HomeWork JS1

--- Firts homework/main.js
+++ Firts homework/main.js
@@ -1,29 +1,60 @@
 let str = "hello";
 console.log(str);
+// alert(str)
+document.write(str,"\n")
+
 let str1 = "owu";
 console.log(str1);
+// alert(str1)
+document.write(str1,"\n")
+
 let str2 = "com";
 console.log(str2);
+// alert(str2)
+document.write(str2,"\n")
+
 let str3 = "ua";
 console.log(str3);
+// alert(str3)
+document.write(str3,"\n")
+
 let nub =  1;
 console.log(nub);
+// alert(nub)
+document.write(nub,"\n")
+
 let nub1 = 10
 console.log(nub1);
+// alert(nub1)
+document.write(nub1,"\n")
 let nub2 = -999;
 console.log(nub2);
+// alert(nub2)
+document.write(nub2,"\n")
 let nub3 = 123;
 console.log(nub3);
+// alert(nub3)
+document.write(nub3,"\n")
 let nub4 = 3.14;
 console.log(nub4);
+// alert(nub4)
+document.write(nub4,"\n")
 let nub5 = 2.7;
 console.log(5);
+// alert(nub5)
+document.write(nub5,"\n")
 let nub6 = 16;
 console.log(nub6);
+// alert(nub6)
+document.write(nub6,"\n")
 let bool = nub3 > nub4;
 console.log(bool);
+// alert(bool)
+document.write(bool,"\n")
 let bool1 = nub6 == nub5;
 console.log(bool1);
+//alert(bool1)
+document.write(bool1,"\n")
 
 let firstName = 'Сергій ';
 let middleName = 'Валерійович ';
