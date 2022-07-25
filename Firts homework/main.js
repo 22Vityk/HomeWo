af2868428cfef63c727dd546d2248d9be30c0ea7
22Vityk <Vityk2904@gmail.com>
2022-07-25 09:57:34 +0300

    Add fils

---
+++ Firts homework/main.js
@@ -0,0 +1,14 @@
+let str = hello;
+let str1 = owu;
+let str2 = com;
+let str3 = ua;
+let nub =  1;
+let nub1 = 10
+let nub2 = -999;
+let nub3 = 123;
+let nub4 = 3.14;
+let nub5 = 2.7;
+let nub6 = 16;
+let bool = true;
+let bool1 = false;
+alert(str,str1,str2,str3)
