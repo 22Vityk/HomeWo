a5ff6684904c28cfc11a1be8b2f19bef7321eb56
22Vityk <Vityk2904@gmail.com>
2022-07-25 13:48:03 +0300

    HomeWork JS1

--- Firts homework/main.js
+++ Firts homework/main.js
@@ -1,14 +1,46 @@
-let str = hello;
-let str1 = owu;
-let str2 = com;
-let str3 = ua;
+let str = "hello";
+console.log(str);
+let str1 = "owu";
+console.log(str1);
+let str2 = "com";
+console.log(str2);
+let str3 = "ua";
+console.log(str3);
 let nub =  1;
+console.log(nub);
 let nub1 = 10
+console.log(nub1);
 let nub2 = -999;
+console.log(nub2);
 let nub3 = 123;
+console.log(nub3);
 let nub4 = 3.14;
+console.log(nub4);
 let nub5 = 2.7;
+console.log(5);
 let nub6 = 16;
-let bool = true;
-let bool1 = false;
-alert(str,str1,str2,str3)
+console.log(nub6);
+let bool = nub3 > nub4;
+console.log(bool);
+let bool1 = nub6 == nub5;
+console.log(bool1);
+
+let firstName = 'Сергій ';
+let middleName = 'Валерійович ';
+let lastName = 'Вітик ';
+let person = lastName + firstName + middleName;
+console.log(person);
+
+let a = 100;
+console.log(typeof a);
+let b = '100';
+console.log(typeof b);
+let c = a === b;
+console.log(typeof c);
+
+let firstNameimput = prompt();
+console.log(firstNameimput);
+let lastNameimput =prompt()
+console.log(lastNameimput)
+let howMuchCM = prompt()
+console.log(howMuchCM)
