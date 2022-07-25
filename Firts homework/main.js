deadf0d195cfb74df2f312ba2c8367d490a34fc5
22Vityk <Vityk2904@gmail.com>
2022-07-25 14:33:57 +0300

    HomeWork JS1

--- Firts homework/main.js
+++ Firts homework/main.js
@@ -27,30 +27,37 @@ let nub1 = 10
 console.log(nub1);
 // alert(nub1)
 document.write(nub1,"\n")
+
 let nub2 = -999;
 console.log(nub2);
 // alert(nub2)
 document.write(nub2,"\n")
+
 let nub3 = 123;
 console.log(nub3);
 // alert(nub3)
 document.write(nub3,"\n")
+
 let nub4 = 3.14;
 console.log(nub4);
 // alert(nub4)
 document.write(nub4,"\n")
+
 let nub5 = 2.7;
 console.log(5);
 // alert(nub5)
 document.write(nub5,"\n")
+
 let nub6 = 16;
 console.log(nub6);
 // alert(nub6)
 document.write(nub6,"\n")
+
 let bool = nub3 > nub4;
 console.log(bool);
 // alert(bool)
 document.write(bool,"\n")
+
 let bool1 = nub6 == nub5;
 console.log(bool1);
 //alert(bool1)
