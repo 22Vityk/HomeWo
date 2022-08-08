0f539fe8e96567068ef49a8bb006e9a35140ac11
22Vityk <Vityk2904@gmail.com>
2022-08-08 19:54:35 +0300

    11

---
+++ Homework 5/main.js
@@ -0,0 +1,70 @@
+//1 - створити функцію яка обчислює та повертає площу прямокутника зі сторонами а і б
+// function calc(a,b){
+//      return a * b
+// }
+//     console.log(calc(12, 20))
+//2 - створити функцію яка обчислює та повертає площу кола з радіусом r
+// function calc(p,r){
+//     return p * (r * r)
+//  }
+//  let s = calc(3.14,5)
+// console.log(s)
+//3 - створити функцію яка обчислює та повертає площу циліндру висотою h, та радіутом r
+// function calc(p,r,h){
+//     return 2 * p * (r * h)
+// }
+// let res = calc(3.14,5,6)
+// console.log(res)
+//4 - створити функцію яка приймає масив та виводить кожен його елемент
+// let users = [
+//     {name: 'vasya', age: 31, status: true},
+//     {name: 'petya', age: 33, status: false},
+//     {name: 'anna', age: 28, status: true},
+//     {name: 'olya', age: 29, status: false}
+// ]
+//     for (const user of users){
+//     }
+// console.log(users)
+//5 - створити функцію яка створює параграф з текстом. Текст задати через аргумент
+// function foo (sdadas){
+//     document.write(`<p>${sdadas}</p>`)
+// }
+//     foo ('sdadas')
+//6 - створити функцію яка створює ul з трьома елементами li. Текст li задати через аргумент всім однаковий
+// function list (arguments){
+//     document.write("<ul>" +
+//         "<li>"+arguments+"</li>" +
+//         "<li>"+arguments+"</li>" +
+//         "<li>"+arguments+"</li>" +
+//         "</ul>")
+// }
+// list("Hello")
+
+//7 - створити функцію яка створює ul з трьома елементами li. Текст li задати через аргумент всім однаковий. Кількість li визначається другим аргументом, який є числовим (тут використовувати цикл)
+// function print_li(pazda,count){
+//     let text = ""
+//     for (let i = 0 ; i < count; i++) {
+//         text += "<li>"+pazda+"</li>";
+//     }
+//     document.write("<ul>"+text+"</ul>")
+// }
+// print_li("Vityk a cat",3)
+//8 - створити функцію яка приймає масив примітивних елементів (числа,стрінги,булеві), та будує для них список
+// let users = [
+//      {name: 'vasya', age: 31, status: true},
+//      {name: 'petya', age: 33, status: false},
+//      {name: 'anna', age: 28, status: true},
+//      {name: 'olya', age: 29, status: false}
+// ]
+// function ddd(){
+//
+// }
+
+//9 - створити функцію яка приймає масив об'єктів з наступними полями id,name,age , та виводить їх в документ. Для кожного об'єкту окремий блок.
+
+
+
+//10 - створити функцію яка повертає найменьше число з масиву
+
+
+// - створити функцію яка приймає масив чисел, сумує значення елементів масиву та повертає його. Приклад [1,2,10]->13
